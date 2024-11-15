e84586e85a62a27ea126614cc031ad8570075433
Tami Elb <Tami.dev@outlook.com>
2024-11-15 11:46:04 +0100

    Init Practicum 2A

---
+++ src/Zwembad.java
@@ -0,0 +1,47 @@
+public class Zwembad {
+    private double breedte;
+    private double lengte;
+    private double diepte;
+
+    public Zwembad() {
+
+    }
+
+    public Zwembad(double breedte, double lengte, double diepte) {
+        this.breedte = breedte;
+        this.lengte = lengte;
+        this.diepte = diepte;
+    }
+
+    public double getBreedte() {
+        return breedte;
+    }
+
+    public void setBreedte(double breedte) {
+        this.breedte = breedte;
+    }
+
+    public double getLengte() {
+        return lengte;
+    }
+
+    public void setLengte(double lengte) {
+        this.lengte = lengte;
+    }
+
+    public double getDiepte() {
+        return diepte;
+    }
+
+    public void setDiepte(double diepte) {
+        this.diepte = diepte;
+    }
+
+    public String toString() {
+        return "Dit zwembad is " + breedte + " meter breed, " + lengte + " meter lang, en " + diepte + " meter diep";
+    }
+
+    public double inhoud() {
+        return diepte + breedte + lengte;
+    }
+}
